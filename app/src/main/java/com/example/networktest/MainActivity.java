7560a9170e1498b35794d67f5dfcb3c6718ee0fd
aronsa <aronsa@edu.aau.at>
2021-03-03 16:33:48 +0100

    new features - layout mainActivity

--- app/src/main/java/com/example/networktest/MainActivity.java
+++ app/src/main/java/com/example/networktest/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.networktest;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sentRequest(View v){
+    }
+
 }
