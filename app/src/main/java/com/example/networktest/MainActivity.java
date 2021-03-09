b6542e18349e11e7a807cdcc5304df4bdb1ea729
aronsa <aronsa@edu.aau.at>
2021-03-09 23:28:29 +0100

    new features - server request

--- app/src/main/java/com/example/networktest/MainActivity.java
+++ app/src/main/java/com/example/networktest/MainActivity.java
@@ -7,7 +7,13 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
+    String serverMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +21,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    class BackgroundThread implements Runnable {
+        String matriculationNumber;
+
+        BackgroundThread(String matriculationNumber) {
+            this.matriculationNumber = matriculationNumber;
+        }
+
+        @Override
+        public void run() {
+            try {
+                String sentence;
+                String modifiedSentence;
+
+                Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+
+                DataOutputStream outToServer = new DataOutputStream((clientSocket.getOutputStream()));
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                sentence = matriculationNumber;
+                outToServer.writeBytes(sentence + '\n');
+                modifiedSentence = inFromServer.readLine();
+
+                System.out.println("FROM SERVER: " + modifiedSentence);
+                serverMessage = modifiedSentence;
+
+                clientSocket.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public void sentRequest(View v) {
+        TextView editText = findViewById(R.id.editText);
+
+        BackgroundThread backgroundThread = new BackgroundThread(editText.getText().toString());
+        Thread t = new Thread(backgroundThread);
+        t.start();
+
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(serverMessage);
     }
 
     public void markDigits(View v) {
